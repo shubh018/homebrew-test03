b06bcb7d726f597f17918fcf8f3f68c0f674a439
Shubham <shubhamveer228@gmail.com>
2023-01-13 04:30:19 +0530

    committed

---
+++ svs.rb
@@ -0,0 +1,12 @@
+class Svs < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/shubh018/testing-0123/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "4aedf12603867c4a036bcff5ae0a67d87781d17bba4c26524eb9bec6d8fbf49f"
+  license ""
+
+  def install
+	bin.install "testrb"
+  end
+
+end
